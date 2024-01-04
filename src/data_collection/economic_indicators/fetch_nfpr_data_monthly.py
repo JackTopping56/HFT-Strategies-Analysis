14bde44328a0d8bae9b9fd5757fe8ecf5b6ddf73
JackTopping56 <jacktopping56@gmail.com>
2024-01-04 11:10:38 +0000

    Collection of Monthly Nonfarm Payroll data

---
+++ src/data_collection/economic_indicators/fetch_nfpr_data_monthly.py
@@ -0,0 +1,44 @@
+import requests
+from datetime import datetime
+import pandas as pd
+from io import StringIO
+
+# Replace 'YOUR_API_KEY_HERE' with your actual Alpha Vantage API key
+api_key = '123Q6N0D536N4R2Y'
+
+
+# Function to fetch and save Nonfarm Payroll data
+def fetch_and_save_nonfarm_payroll(api_key):
+    """Fetches and saves Nonfarm Payroll data for the specified timeframe."""
+    url = f'https://www.alphavantage.co/query?function=NONFARM_PAYROLL&apikey={api_key}&datatype=csv'
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            # Parse the CSV data from the response using StringIO
+            data = pd.read_csv(StringIO(response.text))
+
+            # Assuming 'timestamp' and 'value' are the column headers for date and nonfarm payroll count
+            data['timestamp'] = pd.to_datetime(data['timestamp'])
+
+            # Define the start and end date for filtering
+            start_date = datetime(2020, 12, 1)
+            end_date = datetime(2023, 12, 31)
+
+            # Filter the data based on the date range
+            filtered_data = data[(data['timestamp'] >= start_date) & (data['timestamp'] <= end_date)]
+
+            # Save to CSV
+            file_name = '../../../data/raw/economic_indicators/raw_nfpr_data_monthly/Nonfarm_Payroll_2020-2023.csv'
+            filtered_data.to_csv(file_name, index=False)
+            print(f'Nonfarm Payroll data from Dec 2020 to Dec 2023 saved to {file_name}')
+        else:
+            print(f'Error fetching Nonfarm Payroll data: HTTP Status Code {response.status_code}')
+
+    except Exception as e:
+        print(f'Exception fetching Nonfarm Payroll data: {e}')
+
+
+# Run the function
+fetch_and_save_nonfarm_payroll(api_key)
