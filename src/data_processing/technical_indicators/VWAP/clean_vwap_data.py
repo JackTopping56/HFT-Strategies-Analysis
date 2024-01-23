4de097d259b525fead487b3307211be82ca722a0
JackTopping56 <jacktopping56@gmail.com>
2024-01-23 08:45:01 +0000

    File Refactoring

--- src/data_processing/technical_indicators/VWAP/clean_vwap_data.py
+++ src/data_processing/technical_indicators/VWAP/clean_vwap_data.py
@@ -2,17 +2,16 @@ import os
 from google.cloud import bigquery
 import pandas as pd
 
-service_account_key_path = '/Users/jacktopping/Documents/HFT-Strategies-Analysis/src/data_collection/sentiment_data/lucky-science-410310-ef5253ad49d4.json'
+service_account_key_path = '/src/data_collection/sentiment_data/lucky-science-410310-ef5253ad49d4.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_account_key_path
 
 client = bigquery.Client()
 
-
+# Define your query to load the raw SMA data
 QUERY = (
-    'SELECT * FROM `lucky-science-410310.snp500_technical_indicator_data.snp500_ema_data_raw`'
+    'SELECT * FROM `lucky-science-410310.snp500_technical_indicator_data.snp500_vwap_data_raw`'
 )
 
-# Load the data into a pandas DataFrame
 df = client.query(QUERY).to_dataframe()
 
 print("Data loaded successfully. Number of rows before cleaning:", len(df))
@@ -21,27 +20,27 @@ print("Data loaded successfully. Number of rows before cleaning:", len(df))
 
 # 1. Remove duplicates based on the 'time' column
 df = df.drop_duplicates(subset=['time'])
-print("After removing duplicates. Number of rows:", len(df))
 
 # 2. Handle missing values
 df = df.fillna(method='ffill')
-print("After handling missing values. Number of rows:", len(df))
 
-# 3. Correct data types
+# 3. Correct data types (if needed)
 df['time'] = pd.to_datetime(df['time'])
-df['EMA'] = pd.to_numeric(df['EMA'], errors='coerce')
+df['VWAP'] = pd.to_numeric(df['VWAP'], errors='coerce')
 
 # Handle potential nulls introduced by to_numeric with errors='coerce'
-if df['EMA'].isnull().any():
+if df['VWAP'].isnull().any():
     print("Warning: Non-numeric data found and converted to NaN. Review the original data for errors.")
 
-destination_table_id = 'lucky-science-410310.snp500_technical_indicator_data.snp500_ema_data_clean'
 
+# Define the destination table
+destination_table_id = 'lucky-science-410310.snp500_technical_indicator_data.snp500_vwap_data_clean'
 
+# Define your table schema
 job_config = bigquery.LoadJobConfig(
     schema=[
         bigquery.SchemaField("time", "TIMESTAMP"),
-        bigquery.SchemaField("EMA", "FLOAT"),
+        bigquery.SchemaField("VWAP", "FLOAT"),
     ],
     write_disposition="WRITE_TRUNCATE",  # Overwrites the table if it already exists
 )
@@ -50,4 +49,4 @@ job_config = bigquery.LoadJobConfig(
 job = client.load_table_from_dataframe(df, destination_table_id, job_config=job_config)
 job.result()  # Wait for the job to complete
 
-print("EMA data cleaned and uploaded successfully. Number of rows after cleaning:", len(df))
+print("VWAP data cleaned and uploaded successfully. Number of rows after cleaning:", len(df))
