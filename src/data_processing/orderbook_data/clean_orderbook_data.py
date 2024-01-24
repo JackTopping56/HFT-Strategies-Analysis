65e645c9af55ac64468d9b2d8c1469934b2253e5
JackTopping56 <jacktopping56@gmail.com>
2024-01-24 15:03:38 +0000

    Script that cleans order book data, also converts to dollars and aligns all data to bigquery table

---
+++ src/data_processing/orderbook_data/clean_orderbook_data.py
@@ -0,0 +1,42 @@
+from google.cloud import bigquery
+import os
+
+
+service_account_key_path = '/Users/jacktopping/Documents/HFT-Strategies-Analysis/src/data_collection/sentiment_data/lucky-science-410310-ef5253ad49d4.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_account_key_path
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+# Define query for cleaning the order book data and creating a new table
+clean_query = """
+CREATE OR REPLACE TABLE `lucky-science-410310.snp500_orderbook_data.snp500_orderbook_data_clean` AS
+SELECT
+"""
+
+# Dynamically generate the select statement for all 50 levels
+for i in range(1, 51):
+    clean_query += f"""
+    CAST(`Ask Price {i}` AS FLOAT64) / 10000 AS `Ask Price {i}`,
+    CAST(`Ask Size {i}` AS INT64) AS `Ask Size {i}`,
+    CAST(`Bid Price {i}` AS FLOAT64) / 10000 AS `Bid Price {i}`,
+    CAST(`Bid Size {i}` AS INT64) AS `Bid Size {i}`{"," if i < 50 else ""}
+"""
+
+# Remove the last comma
+clean_query = clean_query.rstrip(',\n')
+
+# Add the FROM clause
+clean_query += """
+FROM 
+  `lucky-science-410310.snp500_orderbook_data.snp500_orderbook_data_raw`
+"""
+
+print(clean_query)
+
+# Run the clean query
+query_job = client.query(clean_query)  # Make an API request.
+
+# Wait for the job to complete.
+query_job.result()
+print("Query results loaded to the table {}".format(query_job.destination))
