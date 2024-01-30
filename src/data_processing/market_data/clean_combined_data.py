8330b161d1ada6bff35164f97029c7761b4936c0
JackTopping56 <jacktopping56@gmail.com>
2024-01-30 09:33:06 +0000

    Script that cleans combined market, economic and technical indicators ready for modelling

---
+++ src/data_processing/market_data/clean_combined_data.py
@@ -0,0 +1,52 @@
+from google.cloud import bigquery
+from google.oauth2 import service_account
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+# Path to your service account key
+service_account_path = '/Users/jacktopping/Documents/HFT-Strategies-Analysis/src/data_collection/sentiment_data/lucky-science-410310-ef5253ad49d4.json'
+
+# Load the credentials
+credentials = service_account.Credentials.from_service_account_file(service_account_path)
+
+# Initialize a BigQuery client with the credentials
+client = bigquery.Client(credentials=credentials)
+
+# Replace this with the actual path to your BigQuery table
+source_table_id = 'lucky-science-410310.snp500_combined_data.combined_market_data'
+destination_table_id = 'lucky-science-410310.snp500_combined_data.combined_market_data_clean'
+
+# Load the data from BigQuery into a DataFrame
+query = f"SELECT * FROM `{source_table_id}`"
+df = client.query(query).to_dataframe()
+
+# Preprocess the Data
+# Convert boolean features to numerical format
+bool_cols = [col for col in df.columns if 'anomaly' in col]
+
+# Handle missing values by forward-filling
+df.fillna(method='ffill', inplace=True)
+
+# Ensure no NAs remain in the anomaly columns, drop rows with NAs in these columns if needed
+df.dropna(subset=bool_cols, inplace=True)
+
+# Now safely convert anomaly columns to integers
+df[bool_cols] = df[bool_cols].astype(int)
+
+# Normalize the volume feature
+scaler = StandardScaler()
+df['volume_scaled'] = scaler.fit_transform(df[['volume']])
+
+# Feature Engineering (e.g., rolling averages, lag features)
+df['rolling_avg_close'] = df['close'].rolling(window=5).mean()
+df['rolling_avg_close'] = df['rolling_avg_close'].shift(1)
+
+# Drop the first few rows that now have NaNs due to rolling calculation
+df.dropna(inplace=True)
+
+# Load the DataFrame back into a new BigQuery table with auto-detected schema
+job_config = bigquery.LoadJobConfig(autodetect=True)
+job = client.load_table_from_dataframe(df, destination_table_id, job_config=job_config)
+job.result()  # Wait for the job to complete
+
+print(f"Loaded {job.output_rows} rows into {destination_table_id}")
