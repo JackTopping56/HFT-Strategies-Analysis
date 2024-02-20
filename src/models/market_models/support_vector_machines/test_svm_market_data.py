79c5b55a4549a282d9fb0e63e9eea5e594afe86e
JackTopping56 <jacktopping56@gmail.com>
2024-02-20 11:50:31 +0000

    Test file for support vector machines

---
+++ src/models/market_models/support_vector_machines/test_svm_market_data.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+from google.cloud import bigquery
+from google.oauth2 import service_account
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+import joblib
+import matplotlib.pyplot as plt
+
+
+credentials = service_account.Credentials.from_service_account_file(
+    '/Users/jacktopping/Documents/HFT-Analysis/src/data_collection/lucky-science-410310-fe46afb2ea6c.json'
+)
+client = bigquery.Client(credentials=credentials)
+
+
+table_id_test = 'lucky-science-410310.final_datasets.market_test_data'
+query_test = f"SELECT * FROM `{table_id_test}`"
+df_test = client.query(query_test).to_dataframe()
+
+# Load the scaler and the trained SVM model
+scaler = joblib.load('scaler_market_svm.joblib')
+model = joblib.load('svm_market_model.joblib')
+
+# Prepare the test dataset
+numeric_features = df_test.select_dtypes(include=[np.number]).columns.tolist()
+features = [col for col in numeric_features if col != 'close']  # Assuming 'close' is the target variable
+X_test = df_test[features].astype(np.float32)
+y_test = df_test['close'].astype(np.float32)
+
+# Standardize the test features using the loaded scaler
+X_test_scaled = scaler.transform(X_test)
+
+# Predict using the loaded SVM model
+y_pred = model.predict(X_test_scaled)
+
+# Calculate and print the Mean Squared Error and Root Mean Squared Error
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+print(f"Test Mean Squared Error: {mse}")
+print(f"Test Root Mean Squared Error: {rmse}")
+
+# Plotting the actual vs predicted values for visual analysis
+plt.figure(figsize=(10, 6))
+plt.scatter(y_test, y_pred, alpha=0.3)
+plt.xlabel('Actual Close Prices')
+plt.ylabel('Predicted Close Prices')
+plt.title('Actual vs Predicted Close Prices')
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=4)
+plt.show()
+
+
+predictions_df = pd.DataFrame({'Actual Close': y_test, 'Predicted Close': y_pred})
+predictions_df.to_csv('market_svm_predictions.csv', index=False)
+print("Predictions saved to 'market_svm_predictions.csv'.")
