a47c13369921ac6fed9fc7c61a83ae1d6ffa8cef
JackTopping56 <jacktopping56@gmail.com>
2024-03-08 10:59:53 +0000

    Removed JSON Key file

--- src/backtesting/market_tests/random_forest_backtest_market.py
+++ src/backtesting/market_tests/random_forest_backtest_market.py
@@ -5,10 +5,8 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 import matplotlib.pyplot as plt
 
-# Initialize Google Cloud credentials and BigQuery client
-credentials = service_account.Credentials.from_service_account_file(
-    '/src/data_collection/lucky-science-410310-fe46afb2ea6c.json')
-client = bigquery.Client(credentials=credentials)
+
+client = bigquery.Client()
 
 # Load the test dataset from BigQuery
 query_test = "SELECT * FROM `lucky-science-410310.final_datasets.market_test_data`"
