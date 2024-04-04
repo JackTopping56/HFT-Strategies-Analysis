69005b78f6e156225589b0cdbaf2b26b81d1fe6b
JackTopping56 <jacktopping56@gmail.com>
2024-04-04 17:40:36 +0100

    Code Cleanup

--- src/data_processing/economic_indicators/clean_cpi_data.py
+++ src/data_processing/economic_indicators/clean_cpi_data.py
@@ -1,7 +1,6 @@
 from google.cloud import bigquery
 import pandas as pd
 
-
 client = bigquery.Client()
 
 QUERY = (
@@ -12,22 +11,19 @@ df = client.query(QUERY).to_dataframe()
 
 print("Data loaded successfully. Number of rows before cleaning:", len(df))
 
-# Basic cleaning steps:
-
 # 1. Remove duplicates based on the 'timestamp' column
 df = df.drop_duplicates(subset=['timestamp'])
 
 # 2. Handle missing values
 df['value'] = df['value'].interpolate(method='linear')
 
-# 3. Correct data types (if needed)
+# 3. Correct data types
 df['timestamp'] = pd.to_datetime(df['timestamp']).dt.date  # Ensuring it's a date format
 df['value'] = pd.to_numeric(df['value'], errors='coerce')
 
 # Handle potential nulls introduced by to_numeric with errors='coerce'
 if df['value'].isnull().any():
     print("Warning: Non-numeric data found and converted to NaN. Review the original data for errors.")
-
 
 destination_table_id = 'lucky-science-410310.snp500_economic_indicator_data.snp500_cpi_data_clean'
 
